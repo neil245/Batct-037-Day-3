ce22295fbc319d8772a84faf0386196d23867f8e
neil245 <indranilghoshal5@gmail.com>
2021-07-30 00:31:36 +0530

    Committed to master

---
+++ src/com/blz/day3/linecomparison/LineComparison.java
@@ -0,0 +1,7 @@
+package com.blz.day3.linecomparison;
+
+public class LineComparison {
+public static void main(String[] args) {
+	System.out.println("Welcome to line comparison computation program");
+}
+}
